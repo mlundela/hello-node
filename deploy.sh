c956849ebc5faba6eb5d843802f4c4aa82aef505
Mads André Lundeland <mads.lundeland@vimond.com>
2016-03-19 15:02:37 +0100

    Add default aws region

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,6 @@ SHA1=$1
 # Create new Elastic Beanstalk version
 EB_BUCKET=hello-bucket
 DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
-AWS_DEFAULT_REGION=eu-central-1
 sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
 aws elasticbeanstalk create-application-version --application-name hello \
